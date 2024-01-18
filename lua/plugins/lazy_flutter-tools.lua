a7900b93a815a57d72e686de1bad2392b7af89f6
Michele Giacomini <michele.giacomini91@gmail.com>
2024-01-18 22:00:19 +0100

    Add metals and dart configuration

---
+++ lua/plugins/lazy_flutter-tools.lua
@@ -0,0 +1,11 @@
+-- NOTE: tools to handle Dart/Flutter projects, including language server
+
+return {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+}
