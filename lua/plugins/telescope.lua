63e680c0ab648a6245eab1d5277b8b52a2472ed7
Michele Giacomini <michele.giacomini91@gmail.com>
2024-02-15 22:15:49 +0100

    Change mappings to navigate telescope results

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    local actions = require("telescope.actions")
+    opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
+      ["<C-k>"] = actions.move_selection_previous,
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-l>"] = actions.preview_scrolling_down,
+      ["<C-h>"] = actions.preview_scrolling_up,
+    })
+  end,
+}
