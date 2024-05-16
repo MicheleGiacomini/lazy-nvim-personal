955856fbe30e50160f8a1cb922a2d2451c5b127e
Michele Giacomini <michele.giacomini91@gmail.com>
2024-05-16 22:06:53 +0200

    use lazyvim extras instead of own config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,16 +3,6 @@ return {
   branch = "dev", -- IMPORTANT!
   keys = {
     {
-      "<leader>xx",
-      "<cmd>Trouble diag_side toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diag_side toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
       "<leader>xe",
       "<cmd>Trouble errors toggle<cr>",
       desc = "Diagnostics (Trouble)",
@@ -22,44 +12,15 @@ return {
       "<cmd>Trouble errors toggle filter.buf=0<cr>",
       desc = "Buffer Diagnostics (Trouble)",
     },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
-    },
   },
   opts = {
     modes = {
-      diag_side = {
-        mode = "diagnostics",
-        preview = {
-          type = "split",
-          relative = "win",
-          position = "right",
-          size = 0.3,
-        }
-      },
       errors = {
-        mode = "diag_side",
+        mode = "diagnostics",
         filter = {
           severity = vim.diagnostic.severity.ERROR,
-        }
-      }
-    }
+        },
+      },
+    },
   }, -- for default options, refer to the configuration section for custom setup.
 }
