e5cfa0913474c240d1ef3bd8fe5509fedcd50daf
Michele Giacomini <michele@quasarud.it>
2024-08-08 10:48:12 +0200

    add some colorschemes, plus change default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,14 @@ return {
   { "sainnhe/everforest" },
   { "rebelot/kanagawa.nvim" },
   { "rose-pine/neovim" },
+  { "folke/tokyonight.nvim" },
+  { "scottmckendry/cyberdream.nvim", },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "tokyonight",
     },
   },
 }
