ab3d893f5dc28507f39f58a3898f96a64c305d30
Michele Giacomini <michele@quasarud.it>
2024-08-30 12:07:46 +0200

    comment out some lsp configs, will need to enable only on personal machine

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,35 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = {
-      ocamllsp = {
-        settings = {
-          codelens = { enable = true },
-        },
-
-        get_language_id = function(_, ftype)
-          return ftype
-        end,
-      },
-    },
-    setup = {
-      ocamllsp = function()
-        local autocmd = vim.api.nvim_create_autocmd
-        local autocmd_clear = vim.api.nvim_clear_autocmds
-        local augroup_codelens = vim.api.nvim_create_augroup("custom-lsp-codelens", { clear = true })
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          autocmd_clear({ group = augroup_codelens, buffer = 0 })
-          autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
-            pattern = { "*.ml" },
-            group = augroup_codelens,
-            callback = function()
-              vim.lsp.codelens.refresh()
-            end,
-            -- buffer = buffer,
-          })
-        end)
-      end,
-    },
+    inlay_hints = { enabled = false },
+    -- servers = {
+    --   ocamllsp = {
+    --     settings = {
+    --       codelens = { enable = true },
+    --     },
+    --
+    --     get_language_id = function(_, ftype)
+    --       return ftype
+    --     end,
+    --   },
+    -- },
+    -- setup = {
+    --   ocamllsp = function()
+    --     local autocmd = vim.api.nvim_create_autocmd
+    --     local autocmd_clear = vim.api.nvim_clear_autocmds
+    --     local augroup_codelens = vim.api.nvim_create_augroup("custom-lsp-codelens", { clear = true })
+    --     require("lazyvim.util").lsp.on_attach(function(_, buffer)
+    --       autocmd_clear({ group = augroup_codelens, buffer = 0 })
+    --       autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
+    --         pattern = { "*.ml" },
+    --         group = augroup_codelens,
+    --         callback = function()
+    --           vim.lsp.codelens.refresh()
+    --         end,
+    --         -- buffer = buffer,
+    --       })
+    --     end)
+    --   end,
+    -- },
   },
 }
