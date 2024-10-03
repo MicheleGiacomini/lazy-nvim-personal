fb7cc3dce981817c3aafa6698a058af90b59404b
Michele Giacomini <michele@quasarud.it>
2024-10-03 13:21:58 +0200

    add no-neck-pain plugin

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,10 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  keys    = {
+    { "<leader>uk", "<cmd>NoNeckPain<cr>", desc = "Center buffer" },
+  },
+  opts    = {
+    width = 150,
+  }
+}
